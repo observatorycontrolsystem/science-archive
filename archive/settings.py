5d42f9a31acea6dd85b58e884c86921739cc2338
Austin Riba <ariba@lcogt.net>
2015-12-11 14:23:31 -0800

    add pgsphere and restframework

--- archive/settings.py
+++ archive/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pgsphere',
+    'rest_framework',
     'django_nose'
 ]
 
