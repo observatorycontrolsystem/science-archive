41e3d9ae71c885ab814c426f4e812eeda8459f2a
Austin Riba <ariba@lcogt.net>
2016-02-24 22:08:55 +0000

    add throttling config

--- archive/settings.py
+++ archive/settings.py
@@ -181,6 +181,14 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    },
     'DEFAULT_METADATA_CLASS': 'archive.frames.metadata.MinimalMetadata',
     'DEFAULT_PAGINATION_CLASS': 'archive.frames.pagination.LimitedLimitOffsetPagination',
 }
