29b844535d86d90f967faeed5010f2764148a006
Austin Riba <ariba@lcogt.net>
2016-06-08 17:46:47 +0000

    Allow recieving auth token via request with bearer token

--- archive/authentication/views.py
+++ archive/authentication/views.py
@@ -1,4 +1,7 @@
 from rest_framework.generics import RetrieveAPIView
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 from archive.authentication.serializers import UserSerializer
 
 
@@ -10,3 +13,12 @@ class UserView(RetrieveAPIView):
             return self.request.user
         else:
             return None
+
+
+class ObtainAuthTokenWithHeaders(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            token, created = Token.objects.get_or_create(user=request.user)
+            return Response({'token': token.key})
+        else:
+            return super().post(request, *args, **kwargs)
--- archive/urls.py
+++ archive/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from rest_framework.authtoken import views as apiviews
 from archive.frames import urls as frame_urls
 from archive.authentication import urls as auth_urls
+from archive.authentication.views import ObtainAuthTokenWithHeaders
 
 urlpatterns = [
     url(r'^', include(frame_urls)),
     url(r'^', include(auth_urls)),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api-token-auth/', apiviews.obtain_auth_token),
+    url(r'^api-token-auth/', ObtainAuthTokenWithHeaders.as_view()),
 ]
