b661d83cf02b803b9af52fc132569c8ab27d66c6
Austin Riba <ariba@lcogt.net>
2016-10-02 00:27:38 +0000

    use timezone instead of datetime in purgequicklook

--- archive/frames/management/commands/purgequicklook.py
+++ archive/frames/management/commands/purgequicklook.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from archive.frames.models import Frame
 from datetime import timedelta
-from datetime import datetime
+from django.utils import timezone
 import logging
 
 logger = logging.getLogger()
@@ -10,5 +10,5 @@ logger = logging.getLogger()
 class Command(BaseCommand):
     def handle(self, *args, **options):
         logger.info('Purging quicklook data')
-        Frame.objects.filter(RLEVEL=10, DATE_OBS__lt=datetime.now(datetime.timezone.utc) - timedelta(days=7)).delete()
-        Frame.objects.filter(RLEVEL=11, DATE_OBS__lt=datetime.now(datetime.timezone.utc) - timedelta(days=7)).delete()
+        Frame.objects.filter(RLEVEL=10, DATE_OBS__lt=timezone.now() - timedelta(days=7)).delete()
+        Frame.objects.filter(RLEVEL=11, DATE_OBS__lt=timezone.now() - timedelta(days=7)).delete()
