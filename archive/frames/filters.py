88620717e254d83b9e9822e482ac4998849940b8
Austin Riba <ariba@lco.global>
2016-11-18 08:40:12 -0800

    v 1.6 upgrade django-filter

--- archive/frames/filters.py
+++ archive/frames/filters.py
@@ -6,16 +6,16 @@ import django_filters
 
 
 class FrameFilter(GeoFilterSet):
-    start = django_filters.DateTimeFilter(name='DATE_OBS', lookup_type='gte')
-    end = django_filters.DateTimeFilter(name='DATE_OBS', lookup_type='lte')
-    covers = geofilters.GeometryFilter(name='area', lookup_type='covers')
-    intersects = geofilters.GeometryFilter(name='area', lookup_type='intersects')
-    basename = django_filters.CharFilter(name='basename', lookup_type='icontains')
-    OBJECT = django_filters.CharFilter(name='OBJECT', lookup_type='icontains')
-    public = django_filters.MethodFilter(action='public_filter')
-    EXPTIME = django_filters.NumberFilter(name='EXPTIME', lookup_type='gte')
+    start = django_filters.DateTimeFilter(name='DATE_OBS', lookup_expr='gte')
+    end = django_filters.DateTimeFilter(name='DATE_OBS', lookup_expr='lte')
+    covers = geofilters.GeometryFilter(name='area', lookup_expr='covers')
+    intersects = geofilters.GeometryFilter(name='area', lookup_expr='intersects')
+    basename = django_filters.CharFilter(name='basename', lookup_expr='icontains')
+    OBJECT = django_filters.CharFilter(name='OBJECT', lookup_expr='icontains')
+    public = django_filters.CharFilter(method='public_filter')
+    EXPTIME = django_filters.NumberFilter(name='EXPTIME', lookup_expr='gte')
 
-    def public_filter(self, queryset, value):
+    def public_filter(self, queryset, name, value):
         if value == 'false':
             return queryset.exclude(L1PUBDAT__lt=timezone.now())
         return queryset
