db6e9ee5f1a140dee5bbfc734e32a9b3e8f3c06e
Austin Riba <ariba@lco.global>
2016-11-29 14:45:01 -0800

    Add method for moving a frame to infrequently accessed storage class

--- archive/frames/models.py
+++ archive/frames/models.py
@@ -134,6 +134,27 @@ class Frame(models.Model):
         client = get_s3_client()
         return client.head_object(Bucket=settings.BUCKET, Key=self.s3_key)['ContentLength']
 
+    def copy_to_ia(self):
+        latest_version = self.version_set.first()
+        client = get_s3_client()
+        logger.info('Copying {} to IA storage'.format(self))
+        response = client.copy_object(
+            CopySource=latest_version.data_params,
+            Bucket=settings.BUCKET,
+            Key=self.s3_key,
+            StorageClass='STANDARD_IA',
+            MetadataDirective='COPY'
+        )
+        new_version = Version(
+            frame=self,
+            key=response['VersionId'],
+            md5=response['CopyObjectResult']['ETag'].strip('"'),
+            extension=latest_version.extension
+        )
+        latest_version.delete()
+        new_version.save()
+        logger.info('Saved new version: {}'.format(new_version.id))
+
 
 class Headers(models.Model):
     data = JSONField(default=dict)
