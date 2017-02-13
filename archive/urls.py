11f5d5e3c162a1a656a1a1b4303343ea773fe518
Austin Riba <ariba@lco.global>
2017-02-13 11:12:12 -0800

    version 1.8 add robots.txt

--- archive/urls.py
+++ archive/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.http import HttpResponse
 from archive.frames import urls as frame_urls
 from archive.authentication import urls as auth_urls
 from archive.authentication.views import ObtainAuthTokenWithHeaders
 
 urlpatterns = [
+    url(r'^robots.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", content_type='text/plain')),
     url(r'^', include(frame_urls)),
     url(r'^', include(auth_urls)),
     url(r'^admin/', admin.site.urls),
