9b7266f55b901d11fd143a180d73d2d7843182bb
Jon <jnation@lco.global>
2020-02-21 00:20:51 +0000

    update expire guide frames a bit

--- archive/frames/management/commands/expireguideframes.py
+++ archive/frames/management/commands/expireguideframes.py
@@ -18,9 +18,21 @@ GUIDE_CAMERAS_TO_PERSIST = get_tuple_from_environment(
 )
 
 class Command(BaseCommand):
+    help = 'Deletes guide frames older than 1 year for imagers'
+
+    def add_arguments(self, parser):
+        parser.add_argument('-s', '--site', type=str, default='all',
+                            help='SITEID to delete guide frames for. Default is all sites.')
+
     def handle(self, *args, **options):
         expiration_date = timezone.now() - timedelta(days=365)
-        logger.info(f'Expiring imager guide frames from before {expiration_date.isoformat()}')
+        logger.info(f"Expiring imager guide frames from before {expiration_date.isoformat()} for {options['site']} site(s)")
 
-        old_guide_frames = Frame.objects.filter(DATE_OBS__lte=expiration_date, OBSTYPE='GUIDE').exclude(
-            INSTRUME__in=GUIDE_CAMERAS_TO_PERSIST).delete()
+        guide_frames = Frame.objects.filter(DATE_OBS__lte=expiration_date, OBSTYPE='GUIDE').exclude(
+            INSTRUME__in=GUIDE_CAMERAS_TO_PERSIST)
+        if options['site'].lower() != 'all':
+            guide_frames = guide_frames.filter(SITEID=options['site'].lower())
+
+        delete_results = guide_frames.delete()
+        for key, val in delete_results[1].items():
+            logger.info(f"Deleted {val} instances of {key}")
