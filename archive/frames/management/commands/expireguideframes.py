2190f92e262af588bd269aa7b37af46c228aa69c
Jon <jnation@lco.global>
2020-02-24 05:49:56 +0000

    delete in batches of 1000, until all frames are deleted

--- archive/frames/management/commands/expireguideframes.py
+++ archive/frames/management/commands/expireguideframes.py
@@ -14,9 +14,9 @@ def get_tuple_from_environment(variable_name, default):
 
 GUIDE_CAMERAS_TO_PERSIST = get_tuple_from_environment(
     'GUIDE_CAMERAS_TO_PERSIST',
-    'kb42,kb38,ak01,ak02,ak03,ak04,ak05,ak06,ak07,ak11,ak12'
+    'kb42,kb38,ak01,ak02,ak03,ak04,ak05,ak06,ak07,ak08,ak09,ak10,ak11,ak12'
 )
-MAX_TO_DELETE = 10000
+DELETE_BATCH = 1000
 
 class Command(BaseCommand):
     help = 'Deletes guide frames older than 1 year for imagers'
@@ -24,19 +24,19 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('-s', '--site', type=str, default='all',
                             help='SITEID to delete guide frames for. Default is all sites.')
-        parser.add_argument('-m', '--max_delete', type=int, default=MAX_TO_DELETE,
-                            help='Max number of frames to delete. Default is 10,000.')
 
     def handle(self, *args, **options):
         expiration_date = timezone.now() - timedelta(days=365)
-        logger.info(f"Expiring up to {options['max_delete']} imager guide frames from before {expiration_date.isoformat()} for {options['site']} site(s)")
+        logger.info(f"Expiring imager guide frames from before {expiration_date.isoformat()} for {options['site']} site(s)")
 
         guide_frames = Frame.objects.filter(DATE_OBS__lte=expiration_date, OBSTYPE='GUIDE').exclude(
             INSTRUME__in=GUIDE_CAMERAS_TO_PERSIST)
         if options['site'].lower() != 'all':
             guide_frames = guide_frames.filter(SITEID=options['site'].lower())
 
-        delete_results = Frame.objects.filter(pk__in=guide_frames.values_list('pk', flat=True)[:options['max_delete']]).delete()
+        while guide_frames.count() > 0:
+            pks_to_delete = guide_frames.values_list('pk', flat=True)[:DELETE_BATCH]
+            delete_results = Frame.objects.filter(pk__in=pks_to_delete).delete()
 
         for key, val in delete_results[1].items():
             logger.info(f"Deleted {val} instances of {key}")
