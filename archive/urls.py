a59bfefa1143cfa446d8716168ae455747bf1f1c
Matt Daily <mdaily@lco.global>
2021-09-13 14:54:27 -0700

    First pass at open api doc generation

--- archive/urls.py
+++ archive/urls.py
@@ -1,9 +1,27 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.urls import path
 from django.http import HttpResponse
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from drf_yasg import openapi
+from django.views.generic import TemplateView
+
 from archive.frames import urls as frame_urls
 from archive.authentication import urls as auth_urls
 from archive.authentication.views import ObtainAuthTokenWithHeaders, HealthCheckView
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Archive API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://lco.global/policies/terms/",
+      contact=openapi.Contact(email="ocs@lco.global"),
+      license=openapi.License(name="GPL 3.0 License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,))
 
 urlpatterns = [
     url(r'^robots.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", content_type='text/plain')),
@@ -12,5 +30,10 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-token-auth/', ObtainAuthTokenWithHeaders.as_view()),
-    url(r'^health/', HealthCheckView.as_view())
+    url(r'^health/', HealthCheckView.as_view()),
+    path('openapi/', schema_view.as_view(), name='openapi-schema'),
+    path('redoc/', TemplateView.as_view(
+        template_name='redoc.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='redoc')
 ]
