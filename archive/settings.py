50019973efca9945a6ac8fc428688651a397a932
Jon <jnation@lco.global>
2021-12-14 07:44:02 +0000

    Fix unit tests by adding back previous configuration types to defaults and set github tests to use test_settings

--- archive/settings.py
+++ archive/settings.py
@@ -237,7 +237,7 @@ PROCESSED_EXCHANGE_NAME = os.getenv('PROCESSED_EXCHANGE_NAME', 'archived_fits')
 
 # Settings for available configuration_types: use configdb if available, otherwise fall back on direct setting
 CONFIGDB_URL = os.getenv('CONFIGDB_URL', '')
-CONFIGURATION_TYPES = get_tuple_from_environment('CONFIGURATION_TYPE', 'BIAS,DARK,EXPOSE,SPECTRUM,LAMPFLAT,SKYFLAT')
+CONFIGURATION_TYPES = get_tuple_from_environment('CONFIGURATION_TYPE', 'BIAS,DARK,EXPOSE,SPECTRUM,LAMPFLAT,SKYFLAT,STANDARD,TRAILED,GUIDE,EXPERIMENTAL,CATALOG')
 
 try:
     from .local_settings import *
