eb08650630702eba8e1325999201f3a2e40b64df
Jon <jnation@lco.global>
2022-03-09 09:33:55 +0000

    Add empty Profile when a new user account is created

--- archive/authentication/apps.py
+++ archive/authentication/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class AuthenticationConfig(AppConfig):
     name = 'archive.authentication'
+
+    def ready(self):
+        import archive.authentication.signals.handlers  # noqa
+        super().ready()
